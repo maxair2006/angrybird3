8cfba45731685fc81bab30e46d3570bd623140a6
maxair2006 <sohamchindarkar58@gmail.com>
2021-01-10 17:38:42 +0530

    constraints

---
+++ Slingshot.js
@@ -0,0 +1,17 @@
+class Slingshot{
+    constructor(body1, body2){
+        var options = {
+            bodyA:body1,
+            bodyB:body2,
+            lenght:30,
+            stiffness:1
+        }
+    this.chain = Constraint.create(options)
+    World.add(world,this.chain)
+    }
+    display(){
+        var pointA = this.chain.bodyA.position
+        var pointB = this.chain.bodyB.position
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var log6;
+var chain;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -34,7 +37,10 @@ function setup(){
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
 
+    log6 = new Log(100,200,20,PI/2)
+
     bird = new Bird(100,100);
+    chain = new Slingshot(bird.body,log6.body);
 
 }
 
@@ -59,6 +65,8 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
+    chain.display();
 
     bird.display();
     fill("purple");
